bfa1b44f4129b0d345b83a9b74336274ca9a600a
maxim.manuylov <maxim.manuylov@gmail.com>
2016-10-25 12:03:03 +0300

    detect network CIDR as well

--- boot/boot.go
+++ boot/boot.go
@@ -15,8 +15,9 @@ func Run() {
     network := flag.String("network", "tcp4", "network to use")
     address := flag.String("address", "", "remote address to connect to")
     timeout := flag.Int("timeout", 5, "timeout in seconds")
-    file := flag.String("file", "", "file to save ip address variable to (optional)")
-    varName := flag.String("var", "PUBLIC_IPV4", "ip address variable name")
+    file := flag.String("file", "", "file to save the variables to (optional)")
+    ipv4VarName := flag.String("ipv4-var", "PUBLIC_IPV4", "IP v4 address variable name")
+    ipv4NetworkVarName := flag.String("ipv4-network-var", "PUBLIC_IPV4_NETWORK", "IP v4 network variable name")
 
     flag.Parse()
 
@@ -35,10 +36,18 @@ func Run() {
     if *file == "" {
         fmt.Fprintf(os.Stdout, "%s", ip)
     } else {
+        ipNetwork, err := FindIPNetwork(ip)
+        if err != nil {
+            application.Exit(err.Error())
+        }
+
         if err := os.MkdirAll(filepath.Dir(*file), os.FileMode(0777)); err != nil {
             application.Exit(fmt.Sprintf("Failed to create parent directory: %v", err))
         }
-        if err := ioutil.WriteFile(*file, []byte(fmt.Sprintf("%s=%s", *varName, ip)), os.FileMode(0666)); err != nil {
+
+        fileText := fmt.Sprintf("%s=%s\n%s=%s", *ipv4VarName, ip, *ipv4NetworkVarName, ipNetwork)
+
+        if err := ioutil.WriteFile(*file, []byte(fileText), os.FileMode(0666)); err != nil {
             application.Exit(fmt.Sprintf("Failed to save file: %v", err))
         }
     }
@@ -59,4 +68,31 @@ func ResolveLocalIP(network, address string, timeout time.Duration) (string, err
     }
 
     return ip, nil
+}
+
+func FindIPNetwork(ipStr string) (string, error) {
+    ip := net.ParseIP(ipStr)
+    if ip == nil {
+        return "", fmt.Errorf("Failed to parse IP address: %s", ipStr)
+    }
+
+    ifaces, err := net.Interfaces()
+    if err != nil {
+        return "", err
+    }
+
+    for _, iface := range ifaces {
+        addrs, err := iface.Addrs()
+        if err != nil {
+            return "", err
+        }
+
+        for _, addr := range addrs {
+            if ipNet, ok := addr.(*net.IPNet); ok && ipNet.Contains(ip) {
+                return ipNet.String(), nil
+            }
+        }
+    }
+
+    return "", fmt.Errorf("Failed to find network interface for IP address: %s", ipStr)
 }
